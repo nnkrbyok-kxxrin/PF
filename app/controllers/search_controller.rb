2b97a030591217fea9a910ba8880aae3c50588cd
nnkrbyok-kxxrin <kxxxx.mxxxkx@gmail.com>
2021-03-04 08:14:12 +0000

    [Start-Finish]search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,16 @@
 class SearchController < ApplicationController
 
   def search
+
+		@model = params[:model]
+		@content = params[:content]
+		@method = params[:method]
+		if @model == 'number'
+			 @records = Guest.number_search_for(@content, @method)
+		elsif @model == 'name'
+					@records = Guest.name_search_for(@content, @method)
+		end
+
   end
 
 end
--- app/models/guest.rb
+++ app/models/guest.rb
@@ -8,4 +8,28 @@ class Guest < ApplicationRecord
     bookmarks.where(admin_id: admin.id).exists?
   end
 
+  def self.number_search_for(content, method)
+      if method == 'perfect'
+        Guest.where(number: content)
+      elsif method == 'forward'
+        Guest.where('number LIKE ?', content + '%')
+      elsif method == 'backward'
+        Guest.where('number LIKE ?', '%' + content)
+      else
+        Guest.where('number LIKE ?', '%' + content + '%')
+      end
+  end
+
+  def self.name_search_for(content, method)
+      if method == 'perfect'
+        Guest.where(name: content)
+      elsif method == 'forward'
+        Guest.where('name LIKE ?', content + '%')
+      elsif method == 'backward'
+        Guest.where('name LIKE ?', '%' + content)
+      else
+        Guest.where('name LIKE ?', '%' + content + '%')
+      end
+  end
+
 end
