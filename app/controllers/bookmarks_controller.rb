d20440f53dcd51b2ab9ca4cf3508e14bc6d1fe73
nnkrbyok-kxxrin <kxxxx.mxxxkx@gmail.com>
2021-03-04 12:39:48 +0000

    [Add]Bookmark-asynchronous communication

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,17 +1,21 @@
 class BookmarksController < ApplicationController
 
   def create
-    guest = Guest.find(params[:guest_id])
-    bookmark = current_admin.bookmarks.new(guest_id: guest.id)
+    @guest = Guest.find(params[:guest_id])
+    # 非同期通信実装のため上記の記述をguest→@guestに変更
+    bookmark = current_admin.bookmarks.new(guest_id: @guest.id)
     bookmark.save
-    redirect_to guests_path(guest)
+    # redirect_to guests_path(guest)
+    # 非同期通信実装のため上記の記述を削除
   end
 
   def destroy
-    guest = Guest.find(params[:guest_id])
-    bookmark = current_admin.bookmarks.find_by(guest_id: guest.id)
+    @guest = Guest.find(params[:guest_id])
+    # 非同期通信実装のため上記の記述をguest→@guestに変更
+    bookmark = current_admin.bookmarks.find_by(guest_id: @guest.id)
     bookmark.destroy
-    redirect_to guests_path(guest)
+    # redirect_to guests_path(guest)
+    # 非同期通信実装のため上記の記述を削除
   end
 
 end
