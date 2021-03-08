45e10f835700a1364d206c0509eb19b7ee6466eb
nnkrbyok-kxxrin <kxxxx.mxxxkx@gmail.com>
2021-03-08 12:01:23 +0000

    [Finish]paging

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,15 +3,14 @@ class SearchController < ApplicationController
   def search
   	# @guests = Guest.all
     # ページング機能実装のため、上記を下記に変更
-    @guests = Guest.page(params[:page]).per(10)
-
+    # @guests = Guest.page(params[:page]).reverse_order
 		@model = params[:model]
 		@content = params[:content]
 		@method = params[:method]
 		if @model == 'number'
-			 @records = Guest.number_search_for(@content, @method)
+			 @records = Guest.number_search_for(@content, @method).page(params[:page]).reverse_order
 		elsif @model == 'name'
-					@records = Guest.name_search_for(@content, @method)
+					@records = Guest.name_search_for(@content, @method).page(params[:page]).reverse_order
 		end
 
   end
