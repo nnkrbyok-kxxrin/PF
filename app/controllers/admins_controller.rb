f9d1bede46eccce0dac2ca12b01a4dcb8b3a6d4e
nnkrbyok-kxxrin <kxxxx.mxxxkx@gmail.com>
2021-04-11 05:11:23 +0000

    [Fix]Flash message

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -4,9 +4,9 @@ class AdminsController < ApplicationController
   def guest_sign_in
     admin = Admin.guest
     sign_in admin
-    redirect_to guests_path, notice: '閲覧用アカウントでログインしました！'
+    redirect_to guests_path, notice: '閲覧用アカウントでログインしました！（※投稿はPF製作者によって作成されたものです）'
   end
-  
+
   def edit
     @admin = Admin.find(params[:id])
   end
@@ -15,8 +15,9 @@ class AdminsController < ApplicationController
     @admin = Admin.find(params[:id])
     if @admin.email != 'guest@example.com'
       if @admin.update(admin_params)
-      # redirect_to edit_admin_path(current_admin.id), notice: 'アカウントの設定を更新しました！'
-      redirect_to guests_path
+      flash.now[:notice] = ''
+      redirect_to edit_admin_path(current_admin.id), alert: 'アカウントの設定を更新しました！'
+      # redirect_to guests_path
       else
         render :edit
       end
